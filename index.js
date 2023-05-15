0687e6f004226aa1760b3e5525ffe09fcc1ad3bb
Andrey <kezin636@gmail.com>
2023-05-15 20:07:03 +0300

    initial

---
+++ index.js
@@ -0,0 +1,7 @@
+import { doAlert } from "./modules/alertModule.js";
+
+const main = async() => {
+    await doAlert("hi to users");
+}
+
+main();
---
+++ modules/alertModule.js
@@ -0,0 +1,5 @@
+const doAlert = async(text) => {
+    alert(`Looks like you wanna alert ${text}`);
+}
+
+export { doAlert }
